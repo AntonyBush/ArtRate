98f82c20160029476ed9bc5f6cb0ff9500e45184
AntonyBush <antonybush95@gmail.com>
2021-10-14 21:38:14 +0530

    Added Upload Form and display images

--- index.php
+++ index.php
@@ -4,14 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="styles/main.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>ArtRate</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <title>Upload Art</title>
 </head>
 <body>
     <?php 
-        include 'nav.php';
+    include 'nav.php';
+    // Include the database configuration file  
+    require_once 'config.php'; 
+    
+    // Get image data from database 
+    $result = $conn->query("SELECT image FROM art"); 
     ?>
+
+    <?php if($result->num_rows > 0){ ?> 
+        <div class="gallery"> 
+            <?php while($row = $result->fetch_assoc()){ ?> 
+                <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" /> 
+            <?php } ?> 
+        </div> 
+    <?php }else{ ?> 
+        <p class="status error">Image(s) not found...</p> 
+    <?php } ?>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </html>
